f00394e734135d51b8d4b8621f11fe5c58f764d7
rickchen911 <45112171+rickchen911@users.noreply.github.com>
2022-02-26 22:26:17 +0800

    This is for Jason

--- main.py
+++ main.py
@@ -4,3 +4,4 @@ from ReadData import getDF
 path = 'E:\\SynologyDrive\\RickUbuntu\\JasonTest\\DataSources\\reviews_Movies_and_TV_5.json.gz'
 if __name__ == '__main__':
     df == getDF()
+    print(df)
